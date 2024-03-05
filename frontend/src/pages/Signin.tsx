7d788604dc4bbf670f3472da13d7c2c424a669f6
oohsai <s72454726@gmail.com>
2024-03-05 06:54:47 +0530

    fixed error

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,7 +1,7 @@
 import { Auth } from "../components/Auth";
 import { Quote } from "../components/Quote";
 
-export default function Signin() {
+export const Signin = () => {
   return (
     <div>
       <div className="grid grid-cols-1 lg:grid-cols-2">
@@ -14,4 +14,4 @@ export default function Signin() {
       </div>
     </div>
   );
-}
+};
