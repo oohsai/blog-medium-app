b55a8eeadc0a9e535f5f1cf2daccc2dc89629d69
oohsai <s72454726@gmail.com>
2024-03-05 07:00:38 +0530

    fixed auth error

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,14 +1,12 @@
-import { SignupInput } from "@tested-demo/honoapp";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { BACKEND_URL } from "../config";
+import { SignupInput } from "@tested-demo/honoapp";
 import axios from "axios";
-
-//trpc - check cohort 1
+import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
   const navigate = useNavigate();
-  const [postInputs, setpostInputs] = useState<SignupInput>({
+  const [postInputs, setPostInputs] = useState<SignupInput>({
     name: "",
     username: "",
     password: "",
@@ -20,96 +18,104 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
         `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
         postInputs
       );
-
       const jwt = response.data;
       localStorage.setItem("token", jwt);
       navigate("/blogs");
-    } catch (error) {
-      console.log(error);
+    } catch (e) {
+      alert("Error while signing up");
+      // alert the user here that the request failed
     }
   }
+
   return (
     <div className="h-screen flex justify-center flex-col">
-      <div className="grid ">
-        <div className="grid justify-center">
-          <div className="text-xl font-bold">Create your account ? </div>
-          <div className="font-light text-slate-200">
-            {type === "signin"
-              ? "Dont have an Account?"
-              : "Already have an account?"}
-            <Link
-              className="pl-2 underline"
-              to={type === "signin" ? "/signup" : "/signin"}
-            >
-              {type === "signin" ? "Sign Up" : "Sign In"}
-            </Link>
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-500">
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Sign up" : "Sign in"}
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="px-10 py-3">
-          {type === "signup" ? (
-            <LabeledInput
-              label="Name"
-              placeholder="John Doe"
-              OnChange={(e) => {
-                setpostInputs({
+          <div className="pt-8">
+            {type === "signup" ? (
+              <LabelledInput
+                label="Name"
+                placeholder="Harkirat Singh..."
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    name: e.target.value,
+                  });
+                }}
+              />
+            ) : null}
+            <LabelledInput
+              label="Username"
+              placeholder="harkirat@gmail.com"
+              onChange={(e) => {
+                setPostInputs({
                   ...postInputs,
-                  name: e.target.value,
+                  username: e.target.value,
                 });
               }}
             />
-          ) : null}
-          <LabeledInput
-            label="Username"
-            placeholder="demo@gmail.com"
-            OnChange={(e) => {
-              setpostInputs({
-                ...postInputs,
-                username: e.target.value,
-              });
-            }}
-          />
-          <LabeledInput
-            label="Password"
-            placeholder="Password"
-            type={"password"}
-            OnChange={(e) => {
-              setpostInputs({
-                ...postInputs,
-                password: e.target.value,
-              });
-            }}
-          />
-          <button
-            onClick={sendRequest}
-            type="button"
-            className=" mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-          >
-            {type === "signup" ? "Sign Up " : "Sign In"}
-          </button>
+            <LabelledInput
+              label="Password"
+              type={"password"}
+              placeholder="123456"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <button
+              onClick={sendRequest}
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign up" : "Sign in"}
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-interface labeledinput {
+interface LabelledInputType {
   label: string;
   placeholder: string;
-  OnChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   type?: string;
 }
 
-function LabeledInput({ label, placeholder, OnChange, type }: labeledinput) {
+function LabelledInput({
+  label,
+  placeholder,
+  onChange,
+  type,
+}: LabelledInputType) {
   return (
     <div>
-      <label className="block mb-2 text-sm font-medium text-black pt-2">
+      <label className="block mb-2 text-sm text-black font-semibold pt-4">
         {label}
       </label>
       <input
+        onChange={onChange}
         type={type || "text"}
-        onChange={OnChange}
         id="first_name"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         placeholder={placeholder}
         required
       />
